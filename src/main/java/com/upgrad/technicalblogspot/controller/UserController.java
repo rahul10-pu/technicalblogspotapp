7f93c4f32ea822cf19145e7b3353008d7c10f710
Abhishek <abhishek1973.cse18@chitkara.edu.in>
2021-02-10 23:05:39 +0530

    adding

--- src/main/java/com/upgrad/technicalblogspot/controller/UserController.java
+++ src/main/java/com/upgrad/technicalblogspot/controller/UserController.java
@@ -1,15 +1,16 @@
 package com.upgrad.technicalblogspot.controller;
 
 
+import com.upgrad.technicalblogspot.model.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-    @RequestMapping
+    @RequestMapping("users/login")
     public String login(Model model){
-        model.addAttribute("user",new User());
-
+        model.addAttribute("user", new user());
+        return "users/login";
     }
 }
